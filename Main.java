c27720d3fada7afbf4db2daf5090547a6f34d3b8
nevereye <itsnevereye@gmail.com>
2024-11-26 13:17:02 -0500

    window

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+public class Main{
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("My First JFrame");
+
+        frame.setSize(300, 200); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
