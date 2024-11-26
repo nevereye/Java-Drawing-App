913d0c28a23effa87ce3eb4f8171bb7f7ea01e45
nevereye <itsnevereye@gmail.com>
2024-11-26 13:24:07 -0500

    update

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import javax.swing.JFrame;
 
 public class Main{
     public static void main(String[] args) {
+        // test
         JFrame frame = new JFrame("My First JFrame");
 
         frame.setSize(300, 200); 
