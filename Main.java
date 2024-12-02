8d51fce0dbc14bab818e5897866c1fdbbe3b389c
nevereye <itsnevereye@gmail.com>
2024-12-02 12:57:43 -0500

    add rect

--- Main.java
+++ Main.java
@@ -1,14 +1,19 @@
 import javax.swing.JFrame;
 import java.awt.Graphics;
 
-public class Main{
+public class Main extends JFrame{
+    public Main(){
+        setTitle("mspaint2");
+        setSize(1600, 900);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    @Override
+    public void paint(Graphics g){
+        g.drawRect(0, 0, 100, 100);
+    }
     public static void main(String[] args) {
-        // test
-        JFrame frame = new JFrame("My First JFrame");
-
-        frame.setSize(300, 200); 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-
+        new Main();
     }
 }
