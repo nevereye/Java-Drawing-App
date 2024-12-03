b0c54ecbad7a66a708f821e41d5c5b975b4fdf18
BuzzyBeeT <155506488+BuzzyBeeT@users.noreply.github.com>
2024-12-03 15:12:14 -0500

    DrawCanvasPanel update - replaced paint method with new paintComponent.

--- DrawCanvasPanel.java
+++ DrawCanvasPanel.java
@@ -4,16 +4,25 @@ import java.awt.*;
 // Chris Note:
 // The User will draw on this JComponent.
 // It will be stored in the main JFrame in the Main class.
+// User draws
 public class DrawCanvasPanel extends JPanel {
     // constructor
     public DrawCanvasPanel()
     {
-
+        
     }
 
     @Override
-    public void paint(Graphics g){
-        g.drawOval(0, 0, 100, 100);
-        repaint();
+    // Update: For painting in java Swing we overide the paintComponent method
+    // We also cast Graphics g as Graphics2D object.
+    // I learned this from textbook and online articles.
+    public void paintComponent(Graphics g){
+        super.paintComponent(g); // pass g to super constructor.
+
+        // cast g as Graphics2D object (Better, more methods)
+        Graphics2D g2d = (Graphics2D)g; 
+
+        g.drawOval(100, 100, 100, 100);
+        //repaint();
     }
 }
