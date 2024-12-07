ef440bb778397a95f9cb5727378300a725892539
BuzzyBeeT <155506488+BuzzyBeeT@users.noreply.github.com>
2024-12-06 23:31:53 -0500

    Fixed merge problem?

--- SideBar.java
+++ SideBar.java
@@ -4,19 +4,10 @@ import java.awt.*;
 public class SideBar extends JPanel{
     private ColorOptionPanel colorPanel;
     private OptionPanel optionPanel;
-<<<<<<< HEAD
 
-    private DrawCanvasPanel SideBarcanvas; // Chris: pass reference of canvas so buttons inside sidebar can modify it???
-
-    public SideBar(DrawCanvasPanel canvas){
-
-        this.SideBarcanvas = canvas; // pass reference to ColorOptionPanel Constructor and OptionPanel Constructor?
-
-        this.setLayout(new GridLayout(2, 1, 5, 5));
-=======
+    
     public SideBar(){
         this.setLayout(new GridLayout(2, 1, 10, 10));
->>>>>>> 7ea38e2e056deb6ccd4079f9c98153f453406743
         this.setBackground(Color.green);
 
         // set preferred size, height does nothing to change panel's size when added to borderLayout. But width widens it.
