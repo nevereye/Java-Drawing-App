1614c3ebd1e588acfb2ac6a0d7df49f32d552cda
BuzzyBeeT <155506488+BuzzyBeeT@users.noreply.github.com>
2024-12-06 20:39:07 -0500

    Widened SideBar Class and gave it Border spacing.

--- SideBar.java
+++ SideBar.java
@@ -7,6 +7,13 @@ public class SideBar extends JPanel{
     public SideBar(){
         this.setLayout(new GridLayout(2, 1, 5, 5));
         this.setBackground(Color.green);
+
+        // set preferred size, height does nothing to change panel's size when added to borderLayout. But width widens it.
+        this.setPreferredSize(new Dimension(200,0));
+
+        // Chris: this adds a "Border" around SideBar, basically adds empty space around component. Helps to visually seperate components.
+        int spacing = 10;
+        this.setBorder(BorderFactory.createMatteBorder(spacing, spacing, spacing, spacing, Color.ORANGE));
         
         // add color panel
         colorPanel = new ColorOptionPanel();
