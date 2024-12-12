603a7d3b245cd4cb70853576c1891a2c36a2eb8b
nevereye <itsnevereye@gmail.com>
2024-12-12 10:33:46 -0500

    update gui

--- OptionPanel.java
+++ OptionPanel.java
@@ -6,29 +6,24 @@ import java.awt.event.*;
 public class OptionPanel extends JPanel{
     JPanel panel1;
     JPanel panel2;
+    JPanel panel3;
     JCheckBox eraser; // toggle "eraser mode" for DrawCanvasPanel on and off.
     JButton clearButton; // clear drawing canvas into a fresh blank sheet.
     JSlider slider;
     public OptionPanel(DrawCanvasPanel canvas){
-        this.setLayout(new GridLayout(2, 1, 5, 5));
+        this.setLayout(new GridLayout(3, 1, 5, 5));
         this.setBackground(Color.red);
         
         panel1 = new JPanel();
         panel1.setBackground(Color.green);
-        panel1.setLayout(new GridLayout(3, 1, 5, 5));
+        panel1.setLayout(new GridLayout(1, 1, 5, 5));
+        panel1.add(new JLabel("Tool Bar: "));
         this.add(panel1);
 
-        panel1.add(new JLabel("Tool Bar: "));
-
-        clearButton = new JButton("clear");
-        clearButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) { 
-                canvas.clearCanvas();
-            }
-        });
-        panel1.add(clearButton, BorderLayout.CENTER);
-
+        panel2 = new JPanel();
+        panel2.setLayout(new GridLayout(1, 2, 5, 5));
+        panel2.setBackground(Color.green);
+        
         eraser = new JCheckBox("eraser");
         eraser.addActionListener(new ActionListener() {
             @Override
@@ -36,7 +31,7 @@ public class OptionPanel extends JPanel{
                 canvas.setEraserMode(eraser.isSelected());
             }
         });
-        panel1.add(eraser);
+        panel2.add(eraser);
         
         slider = new JSlider(1, 20);
         canvas.setBrushSize(slider.getValue());
@@ -46,16 +41,21 @@ public class OptionPanel extends JPanel{
                 canvas.setBrushSize(slider.getValue());
             }
         });
-        panel1.add(slider);
-
-
-        panel2 = new JPanel();
-        panel2.setLayout(new GridLayout(2, 2, 5, 5));
-        panel2.setBackground(Color.green);
-        panel2.add(new JLabel("brush size"));
-        panel2.add(new JButton("+"));
-        panel2.add(new JLabel("brush size"));
-        panel2.add(new JButton("-"));
+        panel2.add(slider);
         this.add(panel2);
+
+        panel3 = new JPanel();
+        panel3.setBackground(Color.green);
+        panel3.setLayout(new GridLayout(1, 1, 5, 5));
+        this.add(panel3);
+        clearButton = new JButton("clear");
+        clearButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) { 
+                canvas.clearCanvas();
+            }
+        });
+        panel3.add(clearButton);
+
     }
 }
